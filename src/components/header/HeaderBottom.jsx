9aadeca76d2f520c5229bae56e40bb4fce46861b
pravinargade18 <pravinargade18@gmail.com>
2023-05-01 15:38:17 +0530

    Added SideBar and HeaderBottom

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,18 +1,90 @@
+import { useEffect, useRef, useState } from "react";
 import { secondHeaderItems } from "../../constants/AllItems";
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiAccountCircleFill } from "react-icons/ri";
+import { GrClose } from "react-icons/gr";
+import SideBarContent from "./SideBarContent";
+import {motion} from 'framer-motion'
+
 const HeaderBottom = () => {
+  const ref=useRef();
+  const [showSideBar, setShowSideBar] = useState(false);
+
+  const showSideBarHandler = () => {
+    setShowSideBar(true);
+  };
+  const hideSideBarHandler = () => {
+    setShowSideBar(false);
+  };
+
+  useEffect(()=>{
+        document.body.addEventListener('click',(e)=>{
+          if(e.target.contains(ref.current)){
+              setShowSideBar(false);
+          }
+        })
+  },[ref]);
   return (
     <div className="w-full px-4 h-[36px]  bg-amazon_light text-white flex items-center ">
       {
         <ul className="flex items-center gap-2 text-sm tracking-wide">
-          <li className="headerHover flex items-center gap-1"><GiHamburgerMenu className="h-5 w-5"/> All</li>
-          {secondHeaderItems.map((item)=>{
-            return <li key={item.id} className="headerHover">{item.title}</li>
+          <li
+            onClick={showSideBarHandler}
+            className="headerHover flex items-center gap-1"
+          >
+            <GiHamburgerMenu className="h-5 w-5" /> All
+          </li>
+          {secondHeaderItems.map((item) => {
+            return (
+              <li key={item.id} className="headerHover">
+                {item.title}
+              </li>
+            );
           })}
         </ul>
       }
+      {/* sidebar starts here */}
+      {showSideBar && (
+        <div className="w-full h-screen text-black fixed top-0 left-0 bg-[rgba(0,0,0,.8)] bg-opacity-50">
+          <div className="w-full h-full relative">
+            <motion.div ref={ref} initial={{x:-400,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:0}} className=" w-[350px] h-full bg-white transition-all  duration-500">
+              <div className="w-full bg-amazon_light text-white py-[12.5px] px-6 flex items-center gap-2">
+                <RiAccountCircleFill className="w-6 h-6" />
+                <h3 className="font font-titleFont font-semibold text-lg tracking-[1px]">
+                  Hello, Sign in
+                </h3>
+              </div>
+              <SideBarContent
+                title="Digital Content & Devices"
+                one="Amazon Music"
+                two="Kindle E-readers & books"
+                three="Amazon Appstore"
+              />
+              <SideBarContent
+                title="Shop By Department"
+                one="Electronics"
+                two="Computers"
+                three="Smart Home"
+              />
+              <SideBarContent
+                title="Programs & Features"
+                one="Gift Cards"
+                three="International Shopping"
+                two="Amazon live"
+              />
+              <SideBarContent
+                title="Help & Settings"
+                one="Your Account"
+                two="Customer Service"
+                three="Contact Us"
+              />
+            <span className="cursor-pointer absolute top-0 left-[360px] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300" onClick={hideSideBarHandler}><GrClose/></span>
+            </motion.div>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default HeaderBottom;
---
+++ src/components/header/SideBarContent.jsx
@@ -0,0 +1,36 @@
+import { HiOutlineChevronRight } from "react-icons/hi";
+
+
+const SideBarContent = ({ title,one,two,three}) => {
+  return (
+    <div className="py-3 border-b-[1px] border-b-gray-300">
+  <h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
+   {title}
+  </h3>
+  <ul className="text-sm">
+    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+      {one}{" "}
+      <span>
+        <HiOutlineChevronRight />
+      </span>
+    </li>
+    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+      {two}{" "}
+      <span>
+        <HiOutlineChevronRight />
+      </span>
+    </li>
+    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+      {three}{" "}
+      <span>
+        <HiOutlineChevronRight />
+      </span>
+    </li>
+  </ul>
+</div>
+  )
+}
+
+export default SideBarContent;
+
+
