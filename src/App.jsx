f3bb704c645df9cc748759a1f3ca7a7dbf12727a
pravinargade18 <pravinargade18@gmail.com>
2023-05-01 18:44:32 +0530

    Added Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./components/Home/Banner"
 import Header from "./components/header/Header"
 
 function App() {
   return (
     <div className="font-bodyFont">
       <Header/>
+      <Banner/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "slick-carousel/slick/slick.css"; 
+
 import App from './App.jsx'
 import './index.css'
 
